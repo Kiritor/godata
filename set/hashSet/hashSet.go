d5d909107a811940b9d062ba148206683d4cbbf0
LCore <kiritor@hotmail.com>
2015-06-30 15:12:44 +0800

    添加Remove方法

--- set/hashSet/hashSet.go
+++ set/hashSet/hashSet.go
@@ -8,10 +8,13 @@ type HashSet struct {
 	m map[interface{}]bool
 }
 
+
+//init HashSetSet
 func NewHashSet() *HashSet {
 	return &HashSet{m:make(map[interface{}]bool)}
 }
 
+//add an element into set
 func (set *HashSet) Add(e interface{}) bool {
 	if !set.m[e] {
 		set.m[e] = true
@@ -19,3 +22,9 @@ func (set *HashSet) Add(e interface{}) bool {
 	}
 	return false
 }
+
+//remove an element from set
+func (set *HashSet) Remove(e interface{}) {
+	delete(set.m,e)
+}
+
