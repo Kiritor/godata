886ca944813b11953ffc226b13cdf6e4da92d044
LCore <kiritor@hotmail.com>
2015-07-28 14:59:04 +0800

    .

--- collection/collection.go
+++ collection/collection.go
@@ -14,28 +14,9 @@ type Collection interface {
 	Size() int
 	//return true if this collection contains no elements
 	IsEmpty() bool
-	//return true if this collection contains the specified element
-	Contains(e interface{})
-	//return an arrayble containing all of the elements in this collection
-	ToArray() []interface{}
-	//return true if the specified element is insert into this collection
-	Add(e interface{}) bool
-	//return true if the specified element is remove from this collection
-	Remove(e interface{}) bool
-	//return true if the collection contains the specified collection
-	ContainsAll(c Collection) bool
-	//return true if the specified collection's elements arerable add to the collection
-	AddAll(c Collection) bool
-	//return true if the specified collection's elements arerable remove from the collection
-	RemoveAll(c Collection) bool
 	//clear the collection
 	Clear()
-	//return true if the specified element equals the collection
-	Equals(e interface{}) bool
-	//return hashCode
-	HashCode() int
+	//return slice of the collection's element
+	Values() []interface{}
 
-
-	//extend Iterable interface
-	iterable.Iterable
 }
